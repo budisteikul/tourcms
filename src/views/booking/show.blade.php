e0f27e4c2e8181bf9667b9bd621a96753f9f329f
Budi <aku@budi.my.id>
2022-03-01 20:20:18 +0700

    Update show.blade.php

--- src/views/booking/show.blade.php
+++ src/views/booking/show.blade.php
@@ -6,15 +6,17 @@
         <div class="col-md-12 pr-0 pl-0 pt-0 pb-0">
              <div class="card">
              
-    <div class="card-header">Booking Detail</div>
+    <div class="card-header">Booking Detail {{ $shoppingcart->confirmation_code }}</div>
     <div class="card-body">
 
             {!! $Content->view_invoice($shoppingcart) !!}
             {!! $Content->view_product_detail($shoppingcart) !!}
             {!! $Booking->get_paymentStatus($shoppingcart) !!}
+            <a class="btn btn-primary" href="{{ env('APP_URL') }}/booking/receipt/{{$shoppingcart->id}}/{{$shoppingcart->session_id}}" target="_blank"><b class="fa fa-eye"></b> View Receipt Page</a>
     </div>
     </div>
     
+        
             </div>
 
         </div>
