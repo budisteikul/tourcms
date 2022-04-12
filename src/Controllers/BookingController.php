ba8fa95d165596af6135a40ec05fcb76ac303e86
Budi <aku@budi.my.id>
2022-04-12 22:19:15 +0700

    Update BookingController.php

--- src/Controllers/BookingController.php
+++ src/Controllers/BookingController.php
@@ -55,7 +55,6 @@ class BookingController extends Controller
         
         $shoppingcart = Cache::get('_'. $sessionId, 'empty');
 
-        print_r($shoppingcart);
 
         if($shoppingcart=="empty")
         {
