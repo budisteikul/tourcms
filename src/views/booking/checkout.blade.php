ec3d4f9e2047b13efb5a1945603c7ec23f4fbf96
Budi <aku@budi.my.id>
2022-06-19 10:49:40 +0700

    Update checkout.blade.php

--- src/views/booking/checkout.blade.php
+++ src/views/booking/checkout.blade.php
@@ -316,7 +316,6 @@ function DELETE()
 <div class="form-group">
 <label for="payment_type"><strong>Payment</strong></label>
 <select style="font-size:16px;height:47px;"  class="form-control" id="payment_type" name="payment_type">
-		<option value="tazapay-promptpay">TAZAPAY PROMPTPAY</option>
 		<option value="tazapay-poli">TAZAPAY POLI</option>
 		<option value="tazapay-paynow">TAZAPAY PAYNOW</option>
 		<option value="dana-dana">DANA</option>
