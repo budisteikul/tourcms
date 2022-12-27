bbf617cf4912a668ea9d3cdf0183750ef142c02b
Budi <aku@budi.my.id>
2022-12-27 11:22:32 +0700

    1

---
+++ src/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace budisteikul\tourcms\Controllers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class TestController extends Controller
+{
+    
+    public function test()
+    {
+        print('aaaa');
+    }
+    
+}
