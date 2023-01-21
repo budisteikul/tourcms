363c032fc45b5040e636637ba75ecc369ac00b50
Budi <aku@budi.my.id>
2023-01-21 16:23:48 +0700

    Update checkout.blade.php

--- src/views/booking/checkout.blade.php
+++ src/views/booking/checkout.blade.php
@@ -327,7 +327,8 @@ function DELETE()
 		<option value="rapyd-sinarmas">SINARMAS BANK TRANSFER IDR</option>
 		<option value="rapyd-fast">DBS FAST TRANSFER SGD</option>
 		<option value="rapyd-paynow">PAYNOW QR SGD</option>
-		<option value="xendit-qris">QRIS</option>
+		<option value="xendit-bss">XENDIT BANK SAHABAT SAMPOERNA</option>
+		<option value="xendit-qris">XENDIT QRIS</option>
 		<!-- option value="tazapay-poli">TAZAPAY POLI</option>
 		<option value="tazapay-paynow">TAZAPAY PAYNOW</option>
         <option value="rapyd-poli">RAPYD POLI</option>
