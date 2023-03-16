73faa9aed035c9cbe0ff3a32ee9b0a22ef0040dc
Budi <aku@budi.my.id>
2023-03-16 11:40:11 +0700

    Update checkout.blade.php

--- src/views/booking/checkout.blade.php
+++ src/views/booking/checkout.blade.php
@@ -324,6 +324,7 @@ function DELETE()
 <div class="form-group">
 <label for="payment_type"><strong>Payment</strong></label>
 <select style="font-size:16px;height:47px;"  class="form-control" id="payment_type" name="payment_type">
+		<option value="finmo-acct">FINMO ACCT (not work)</option>
 		<option value="none">NONE</option>
 		<option value="finmo-npp">FINMO NPP</option>
 		<option value="rapyd-creditcard">RAPYD CREDIT CARD</option>
