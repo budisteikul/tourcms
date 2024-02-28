fe2c921b97de32ce5ee439d35c509c54b8b8207c
Budi <aku@budi.my.id>
2024-02-28 10:53:52 +0700

    Update show.blade.php

--- src/views/booking/show.blade.php
+++ src/views/booking/show.blade.php
@@ -30,7 +30,7 @@
             <div class="card mb-2 ">
                 <div class="card-header bg-secondary">CUSTOMER</div>
                   <ul class="list-group list-group-flush ml-0 mr-0 pl-0 pr-0">
-                    <li class="list-group-item"><a href="/api/pdf/invoice/{{ $shoppingcart->session_id }}/Invoice-{{ $shoppingcart->confirmation_code }}.pdf">Invoide-{{ $shoppingcart->confirmation_code }}.pdf</a></li>
+                    <li class="list-group-item"><a href="/api/pdf/invoice/{{ $shoppingcart->session_id }}/Invoice-{{ $shoppingcart->confirmation_code }}.pdf"><i class="far fa-file-pdf"></i> Invoice-{{ $shoppingcart->confirmation_code }}.pdf</a></li>
                     <li class="list-group-item"><b>Name :</b> {{ $contact->firstName }} {{ $contact->lastName }}<input type="hidden" id="full_name" value="{{ $contact->firstName }} {{ $contact->lastName }}"> <button onclick="copyToClipboard('#full_name')" title="Copied" data-toggle="tooltip" data-placement="right" data-trigger="click" class="btn btn-light btn-sm invoice-hilang"><i class="far fa-copy"></i></button></li>
                     <li class="list-group-item"><b>Phone :</b> {{ $contact->phoneNumber }}</li>
                     <li class="list-group-item"><b>Email :</b> {{ $contact->email }}</li>
@@ -80,27 +80,11 @@
 
             @if(Auth::user()->id==1)
             @if($shoppingcart->booking_status!="CANCELED")
-                @if($shoppingcart->shoppingcart_payment->payment_provider!="none")
-                    @if($shoppingcart->shoppingcart_payment->payment_status==4)
             <div class="card mb-2">
                 <div class="card-body bg-light">
                     <button id="btn-del" type="button" onClick="CANCEL('{{ $shoppingcart->id }}','{{ $shoppingcart->confirmation_code }}'); return false;" class="btn btn-block btn-danger mr-0"><i class="fa fa-ban"></i> <b>Cancel this booking</b></button>
                 </div>
             </div>
-                    @else
-            <div class="card mb-2">
-                <div class="card-body bg-light">
-                    <button id="btn-ref" type="button" onClick="REFUND('{{ $shoppingcart->id }}','{{ $shoppingcart->session_id }}','{{ $shoppingcart->confirmation_code }}'); return false;" class="btn btn-block btn-danger mr-0"><i class="fa fa-ban"></i> <b>Cancel and refund this booking</b></button>
-                </div>
-            </div>
-                    @endif
-                @else
-            <div class="card mb-2">
-                <div class="card-body bg-light">
-                    <button id="btn-del" type="button" onClick="CANCEL('{{ $shoppingcart->id }}','{{ $shoppingcart->confirmation_code }}'); return false;" class="btn btn-block btn-danger mr-0"><i class="fa fa-ban"></i> <b>Cancel this booking</b></button>
-                </div>
-            </div>
-                @endif
             @endif
             
             @endif
