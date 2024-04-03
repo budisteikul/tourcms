f9dbe22b4b58fdae10ccc0e97fe374244567fa8b
Budi <aku@budi.my.id>
2024-04-04 00:29:56 +0700

    1

--- src/Controllers/ContactController.php
+++ src/Controllers/ContactController.php
@@ -13,6 +13,7 @@ use budisteikul\coresdk\Models\FileTemp;
 use budisteikul\toursdk\Models\Contact;
 use budisteikul\toursdk\Models\Message;
 use budisteikul\toursdk\Helpers\WhatsappHelper;
+use budisteikul\toursdk\Helpers\GeneralHelper;
 
 class ContactController extends Controller
 {
@@ -21,7 +22,51 @@ class ContactController extends Controller
         $id = $request->input('id');
         $contact = Contact::where('id',$id)->firstOrFail();
         $messages = Message::where('contact_id',$contact->id)->orderBy('created_at','desc')->get();
-        return view('tourcms::contact.message',['messages'=>$messages]);
+
+        $output = '';
+        foreach($messages as $message)
+        {
+            $style1 = 'card bg-light mb-2';
+            if($message->from==null)
+            {
+                $style1 = 'card text-white bg-success mb-2';
+            }
+
+            $message_text = '';
+            if($message->type=="text")
+            {
+                $message_text = json_decode($message->text)->body;
+            }
+
+            if($message->type=="image")
+            {
+                $image = json_decode($message->image);
+                $image_link = '';
+                if(isset($image->link)) $image_link = $image->link;
+                $image_text = '<img src="'.$image_link.'" class="img-thumbnail mb-2" style="max-height: 100px;">';
+                $message_text = $image_text;
+                if(isset($image->caption)) $message_text = $image_text.'<br />'. $image->caption;
+            }
+
+            if($message->type=="reaction")
+            {
+                $message_text = json_decode($reaction->text)->emoji;
+            }
+
+            if($message->type=="template")
+            {
+                $message_text = json_decode($message->template)->name;
+            }
+
+            $output .= '<div class="'.$style1.'" >
+                            <div class="card-body">
+                                <p class="card-text mb-0">'. nl2br($message_text) .'</p>
+                                <small>'.GeneralHelper::dateFormat($message->created_at,2).'</small>
+                            </div>
+                        </div>';
+        }
+
+        return view('tourcms::contact.message',['output'=>$output]);
     }
 
     public function template(Request $request)
