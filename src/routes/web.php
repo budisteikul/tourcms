86b9031f2fe1dfc86f360dcfbd8d3f311708788c
Budi <aku@budi.my.id>
2024-09-12 01:30:14 +0700

    Update web.php

--- src/routes/web.php
+++ src/routes/web.php
@@ -13,6 +13,15 @@ Route::resource('/cms/booking','budisteikul\tourcms\Controllers\BookingControlle
 Route::resource('/cms/schedule','budisteikul\tourcms\Controllers\ScheduleController',[ 'names' => 'route_tourcms_schedule' ])
 	->middleware(['web','auth','verified','CoreMiddleware','SettingMiddleware']);
 
+//Shoppingcart
+	Route::get('/api/activity/{activityId}/calendar/json/{year}/{month}', 'budisteikul\tourcms\Controllers\APIController@snippetscalendar')->middleware(['SettingMiddleware']);
+	Route::post('/api/activity/invoice-preview', 'budisteikul\tourcms\Controllers\APIController@snippetsinvoice')->middleware(['SettingMiddleware']);
+	Route::post('/api/activity/remove', 'budisteikul\tourcms\Controllers\APIController@removebookingid')->middleware(['SettingMiddleware']);
+	Route::post('/api/widget/cart/session/{id}/activity', 'budisteikul\tourcms\Controllers\APIController@addshoppingcart')->middleware(['SettingMiddleware']);
+	Route::post('/api/shoppingcart', 'budisteikul\tourcms\Controllers\APIController@shoppingcart')->middleware(['SettingMiddleware']);
+	Route::post('/api/promocode', 'budisteikul\tourcms\Controllers\APIController@applypromocode')->middleware(['SettingMiddleware']);
+	Route::post('/api/promocode/remove', 'budisteikul\tourcms\Controllers\APIController@removepromocode')->middleware(['SettingMiddleware']);
+
 //Completed
 Route::resource('/cms/completed','budisteikul\tourcms\Controllers\CompletedController',[ 'names' => 'route_tourcms_completed' ])
 	->middleware(['web','auth','verified','CoreMiddleware','SettingMiddleware']);
@@ -73,6 +82,94 @@ Route::resource('/cms/partner','budisteikul\tourcms\Controllers\PartnerControlle
 //Download vCard
 Route::get('/cms/vcard/download/{id}','budisteikul\tourcms\Controllers\VCardController@index')->middleware(['web','auth','verified','CoreMiddleware','SettingMiddleware']);
 
+// ==================================
+// API
+	Route::get('/api/index_jscript', 'budisteikul\tourcms\Controllers\APIController@config')->middleware(['SettingMiddleware']);
+	Route::get('/api/config', 'budisteikul\tourcms\Controllers\APIController@config')->middleware(['SettingMiddleware']);
+	Route::get('/api/{sessionId}/navbar', 'budisteikul\tourcms\Controllers\APIController@navbar')->middleware(['SettingMiddleware']);
+	Route::get('/api/tawkto/{id}', 'budisteikul\tourcms\Controllers\APIController@tawkto')->middleware(['SettingMiddleware']);
 
+	//Review
+	Route::post('/api/review', 'budisteikul\tourcms\Controllers\APIController@review')->middleware(['SettingMiddleware']);
+	Route::get('/api/review/count', 'budisteikul\tourcms\Controllers\APIController@review_count')->middleware(['SettingMiddleware']);
+	Route::get('/api/review/jscript', 'budisteikul\tourcms\Controllers\APIController@review_jscript')->middleware(['SettingMiddleware']);
+
+	//Schedule
+	Route::post('/api/schedule', 'budisteikul\tourcms\Controllers\APIController@schedule')->middleware(['SettingMiddleware']);
+	Route::get('/api/schedule/jscript', 'budisteikul\tourcms\Controllers\APIController@schedule_jscript')->middleware(['SettingMiddleware']);
+
+	//Page
+	Route::get('/api/page/{slug}', 'budisteikul\tourcms\Controllers\APIController@page')->middleware(['SettingMiddleware']);
+
+	//Category
+    Route::get('/api/categories', 'budisteikul\tourcms\Controllers\APIController@categories')->middleware(['SettingMiddleware']);
+    Route::get('/api/category/{slug}', 'budisteikul\tourcms\Controllers\APIController@category')->middleware(['SettingMiddleware']);
+
+    //Product
+    Route::post('/api/product/add', 'budisteikul\tourcms\Controllers\APIController@product_add')->middleware(['SettingMiddleware']);
+    Route::post('/api/product/remove', 'budisteikul\tourcms\Controllers\APIController@product_remove')->middleware(['SettingMiddleware']);
+    Route::get('/api/product/{slug}', 'budisteikul\tourcms\Controllers\APIController@product')->middleware(['SettingMiddleware']);
+	Route::get('/api/product/{slug}/{sessionId}/product_jscript', 'budisteikul\tourcms\Controllers\APIController@product_jscript')->middleware(['SettingMiddleware']);
+
+
+
+
+
+	//Create Payment
+	Route::post('/api/payment/checkout', 'budisteikul\tourcms\Controllers\PaymentController@checkout')->middleware(['SettingMiddleware']);
+	//Stripe
+	Route::get('/api/payment/stripe/jscript/{sessionId}', 'budisteikul\tourcms\Controllers\PaymentController@stripe_jscript')->middleware(['SettingMiddleware']);
+	Route::post('/api/payment/stripe', 'budisteikul\tourcms\Controllers\PaymentController@createpaymentstripe')->middleware(['SettingMiddleware']);
+	//Xendit
+	Route::get('/api/payment/xendit/jscript/{sessionId}', 'budisteikul\tourcms\Controllers\PaymentController@xendit_jscript')->middleware(['SettingMiddleware']);
+	Route::post('/api/payment/xendit', 'budisteikul\tourcms\Controllers\PaymentController@createpaymentxendit')->middleware(['SettingMiddleware']);
+	//Paypal
+	Route::get('/api/payment/paypal/jscript/{sessionId}', 'budisteikul\tourcms\Controllers\PaymentController@paypal_jscript')->middleware(['SettingMiddleware']);
+	Route::post('/api/payment/paypal', 'budisteikul\tourcms\Controllers\PaymentController@createpaymentpaypal')->middleware(['SettingMiddleware']);
+	//QRIS
+	Route::get('/api/payment/qris/jscript/{sessionId}', 'budisteikul\tourcms\Controllers\PaymentController@qris_jscript')->middleware(['SettingMiddleware']);
+	//Wise
+	Route::get('/api/payment/wise/jscript/{sessionId}', 'budisteikul\tourcms\Controllers\PaymentController@wise_jscript')->middleware(['SettingMiddleware']);
+
+	//Checkout
+	Route::get('/api/checkout/jscript', 'budisteikul\tourcms\Controllers\APIController@checkout_jscript')->middleware(['SettingMiddleware']);
+
+	//Receipt
+	Route::get('/api/receipt/jscript', 'budisteikul\tourcms\Controllers\APIController@receipt_jscript')->middleware(['SettingMiddleware']);
+	Route::get('/api/receipt/{sessionId}/{confirmationCode}', 'budisteikul\tourcms\Controllers\APIController@receipt')->middleware(['SettingMiddleware']);
+
+	//Cancellation
+	Route::post('/api/cancel/{sessionId}/{confirmationCode}', 'budisteikul\tourcms\Controllers\APIController@cancellation')->middleware(['SettingMiddleware']);
+
+	//Callback Payment
+	Route::post('/api/payment/stripe/confirm', 'budisteikul\tourcms\Controllers\CallbackController@confirmpaymentstripe')->middleware(['SettingMiddleware']);
+	Route::post('/api/payment/paypal/confirm', 'budisteikul\tourcms\Controllers\CallbackController@confirmpaymentpaypal')->middleware(['SettingMiddleware']);
+	Route::post('/api/payment/xendit/confirm', 'budisteikul\tourcms\Controllers\CallbackController@confirmpaymentxendit')->middleware(['SettingMiddleware']);
+	
+	//PDF
+	Route::get('/api/pdf/manual/{sessionId}/Manual-{id}.pdf', 'budisteikul\tourcms\Controllers\APIController@manual')->middleware(['SettingMiddleware']);
+	Route::get('/api/pdf/invoice/{sessionId}/Invoice-{id}.pdf', 'budisteikul\tourcms\Controllers\APIController@invoice')->middleware(['SettingMiddleware']);
+	Route::get('/api/pdf/ticket/{sessionId}/Ticket-{id}.pdf', 'budisteikul\tourcms\Controllers\APIController@ticket')->middleware(['SettingMiddleware']);
+	Route::get('/api/pdf/instruction/{sessionId}/Instruction-{id}.pdf', 'budisteikul\tourcms\Controllers\APIController@instruction')->middleware(['SettingMiddleware']);
+
+	//Download
+	Route::get('/api/qrcode/{sessionId}/{id}', 'budisteikul\tourcms\Controllers\APIController@downloadQrcode')->middleware(['SettingMiddleware']);
+
+	//Last Order
+	Route::get('/api/ticket/{sessionId}/last-order', 'budisteikul\tourcms\Controllers\APIController@last_order')->middleware(['SettingMiddleware']);
+
+	// Webhook
+	Route::post('/webhook/{webhook_app}', 'budisteikul\tourcms\Controllers\WebhookController@webhook')->middleware(['SettingMiddleware']);
+	Route::get('/webhook/{webhook_app}', 'budisteikul\tourcms\Controllers\WebhookController@webhook')->middleware(['SettingMiddleware']);
+
+	//TASK
+	Route::post('/task', 'budisteikul\tourcms\Controllers\TaskController@task')->middleware(['SettingMiddleware']);
+
+	//LOG
+	Route::post('/logger/{identifier}', 'budisteikul\tourcms\Controllers\LogController@log')->middleware(['SettingMiddleware']);
+
+	//Billing Tools
+	Route::post('/api/tool/billing/{sessionId}', 'budisteikul\tourcms\Controllers\ToolController@billing')->middleware(['SettingMiddleware']);
+	Route::post('/api/tool/bin', 'budisteikul\tourcms\Controllers\ToolController@bin')->middleware(['SettingMiddleware']);
 
 Route::get('/cms/test','budisteikul\tourcms\Controllers\BookingController@test')->middleware(['web','auth','verified','CoreMiddleware','SettingMiddleware']);
