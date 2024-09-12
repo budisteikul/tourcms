a5521a3c39cc0654302819983e7f931c48d640cc
Budi <aku@budi.my.id>
2024-09-13 01:17:00 +0700

    1

---
+++ src/Controllers/FinCategoryController.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace budisteikul\tourcms\Controllers;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+use budisteikul\tourcms\DataTables\CategoriesDataTable;
+use Illuminate\Support\Facades\Validator;
+use budisteikul\tourcms\Models\fin_categories;
+use budisteikul\tourcms\Models\fin_transactions;
+use budisteikul\tourcms\Classes\FinClass;
+
+class FinCategoryController extends Controller
+{
+    public function structure()
+    {
+        $root_categories = fin_categories::where('parent_id',0)->orderBy('name')->get();
+        return view('tourcms::fin.categories.show',['root_categories'=>$root_categories]);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(FinCategoriesDataTable $dataTable)
+    {
+        return $dataTable->render('tourcms::fin.categories.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = FinClass::getCategories();
+        return view('tourcms::fin.categories.create',['categories'=>$categories]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+          	'name' => 'required|string|max:255',
+       	]);
+        
+       	if ($validator->fails()) {
+            $errors = $validator->errors();
+			return response()->json($errors);
+       	}
+		
+		$name =  $request->input('name');
+		$type =  $request->input('type');
+		$parent_id =  $request->input('parent_id');
+
+		$fin_categories = new fin_categories();
+		$fin_categories->name = $name;
+        $fin_categories->parent_id = $parent_id;
+		if($parent_id>0) {
+            $type = fin_categories::select(['type'])->where('id', $parent_id)->first();
+            $fin_categories->type = $type->type;
+        } else {
+            $fin_categories->type = $type;
+        }
+		$fin_categories->save();
+		
+		return response()->json([
+					"id" => "1",
+					"message" => 'Success'
+				]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(fin_categories $category)
+    {
+        $categories = FinClass::getCategories(true,$category->id);
+        return view('tourcms::fin.categories.edit',['category'=>$category,'categories'=>$categories]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+          	'name' => 'required|string|max:255',
+       	]);
+        
+       	if ($validator->fails()) {
+            $errors = $validator->errors();
+			return response()->json($errors);
+       	}
+		
+		$name =  $request->input('name');
+		$type =  $request->input('type');
+        $parent_id =  $request->input('parent_id');
+		
+		$fin_categories = fin_categories::findOrFail($id);
+		$fin_categories->name = $name;
+        $fin_categories->parent_id = $parent_id;
+        
+
+        if($parent_id>0) {
+            $type = fin_categories::select(['type'])->where('id', $parent_id)->first();
+            $fin_categories->type = $type->type;
+        } else {
+            $fin_categories->type = $type;
+        }
+		
+        
+
+		$fin_categories->save();
+
+        $categories = FinClass::getChild($fin_categories->id);
+        foreach($categories as $category)
+        {
+            fin_categories::where('id',$category)->update(['type' => $fin_categories->type]);
+        }
+		
+		return response()->json([
+					"id" => "1",
+					"message" => 'Success'
+				]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(fin_categories $category)
+    {
+        fin_categories::where('parent_id',$category->id)->update(['parent_id'=>0]);
+        fin_transactions::where('category_id', $category->id)->delete();
+        $category->delete();
+		
+    }
+}
---
+++ src/DataTables/FinCategoriesDataTable.php
@@ -0,0 +1,120 @@
+<?php
+namespace budisteikul\tourcms\DataTables;
+
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+use budisteikul\tourcms\Models\fin_categories;
+use budisteikul\tourcms\Classes\FinClass;
+
+
+class FinCategoriesDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return datatables($query)
+            ->addIndexColumn()
+            ->addColumn('detail', function($id){
+                    return FinClass::nameCategory($id->id,"-");
+                })
+			->addColumn('action', function ($id) {
+                if(FinClass::haveTransaction($id->id))
+                {
+                    return '<div class="btn-toolbar justify-content-end"><div class="btn-group mr-2 mb-0" role="group"><button id="btn-edit" type="button" onClick="EDIT(\''.$id->id.'\'); return false;" class="btn btn-sm btn-success pt-0 pb-0 pl-1 pr-1"><i class="fa fa-edit"></i> Edit</button></div><div class="btn-group mb-2" role="group"></div></div>';
+                }
+                else
+                {
+                    return '<div class="btn-toolbar justify-content-end"><div class="btn-group mr-2 mb-0" role="group"><button id="btn-edit" type="button" onClick="EDIT(\''.$id->id.'\'); return false;" class="btn btn-sm btn-success pt-0 pb-0 pl-1 pr-1"><i class="fa fa-edit"></i> Edit</button><button id="btn-del" type="button" onClick="DELETE(\''. $id->id .'\')" class="btn btn-sm btn-danger pt-0 pb-0 pl-1 pr-1"><i class="fa fa-trash-alt"></i> Delete</button></div><div class="btn-group mb-2" role="group"></div></div>';
+                }
+            })
+			->rawColumns(['action']);
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Fin/CategoriesDataTable $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(fin_categories $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->parameters([
+						'language' => [
+							'paginate' => [
+								'previous'=>'<i class="fa fa-step-backward"></i>',
+								'next'=>'<i class="fa fa-step-forward"></i>',
+								'first'=>'<i class="fa fa-fast-backward"></i>',
+								'last'=>'<i class="fa fa-fast-forward"></i>'
+								]
+							],
+						'pagingType' => 'full_numbers',
+						'responsive' => true,
+						'order' => [0,'desc']
+                    ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    public function getColumns(): array
+    {   
+        return [
+            Column::make('created_at')
+                  ->visible(false)
+                  ->searchable(false),
+            Column::computed('DT_RowIndex')
+                  ->width(30)
+                  ->title('No')
+                  ->orderable(false)
+                  ->searchable(false)
+                  ->addClass('text-center align-middle'),
+            Column::make('id')->title('ID')->orderable(false)->addClass('align-middle'),
+            Column::make('name')->title('Name')->orderable(false)->addClass('align-middle'),
+            Column::make('detail')->title('Detail')->orderable(false)->addClass('align-middle'),
+            Column::make('type')->title('Type')->orderable(false)->addClass('align-middle'),
+            
+            Column::computed('action')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(220)
+                  ->addClass('text-center'),
+            
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename(): string
+    {
+        return 'Fin/Categories_' . date('YmdHis');
+    }
+}
