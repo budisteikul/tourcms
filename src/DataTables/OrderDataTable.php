ae1a6bacb51cc18218d5764077ddfa68fe317ae8
Budi <aku@budi.my.id>
2025-04-08 17:47:22 +0700

    1

---
+++ src/DataTables/OrderDataTable.php
@@ -0,0 +1,136 @@
+<?php
+namespace budisteikul\tourcms\DataTables;
+
+use budisteikul\tourcms\Models\Channel;
+
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+use budisteikul\tourcms\Helpers\ReviewHelper;
+
+class ChannelDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return datatables($query)
+                ->addIndexColumn()
+                ->editColumn('invoice', function ($id) {
+                    if($id->invoice==1)
+                    {
+                        return 'Before the tour';
+                    }
+                    else
+                    {
+                        return 'After the tour';
+                    }
+                }) 
+                ->addColumn('action', function ($id) {
+
+                $button_delete = '<button id="btn-del" type="button" onClick="DELETE(\''. $id->id .'\')" class="btn btn-sm btn-danger pt-0 pb-0 pl-1 pr-1"><i class="fa fa-trash-alt"></i> Delete</button>';
+                $button_edit = '<button id="btn-edit" type="button" onClick="EDIT(\''.$id->id.'\'); return false;" class="btn btn-sm btn-success pt-0 pb-0 pl-1 pr-1"><i class="fa fa-edit"></i> Edit</button>';
+                if(ReviewHelper::channel_have_review($id)) $button_delete = '';
+                return '
+                <div class="btn-toolbar justify-content-end">
+                    <div class="btn-group mr-2" role="group">
+                        
+                        '. $button_edit .'
+
+                        '. $button_delete .'
+                        
+                        
+                    </div>
+                </div>';
+                })
+                ->rawColumns(['action','description']);
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\App\ChannelDataTable $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Channel $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->parameters([
+                        'language' => [
+                            'paginate' => [
+                                'previous'=>'<i class="fa fa-step-backward"></i>',
+                                'next'=>'<i class="fa fa-step-forward"></i>',
+                                'first'=>'<i class="fa fa-fast-backward"></i>',
+                                'last'=>'<i class="fa fa-fast-forward"></i>'
+                                ]
+                            ],
+                        'pagingType' => 'full_numbers',
+                        'responsive' => true,
+                        'order' => [0,'desc']
+                    ]);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('created_at')
+                  ->visible(false)
+                  ->searchable(false),
+            Column::computed('DT_RowIndex')
+                  ->width(30)
+                  ->title('No')
+                  ->orderable(false)
+                  ->searchable(false)
+                  ->addClass('text-center align-middle'),
+
+            Column::make('name')->title('Name')->orderable(false)->addClass('align-middle'),
+            Column::make('description')->title('Description')->orderable(false)->addClass('align-middle'),
+            Column::make('invoice')->title('Invoice')->orderable(false)->addClass('align-middle'),
+            
+            
+            Column::computed('action')
+                  ->exportable(false)
+                  ->printable(false)
+                  ->width(220)
+                  ->addClass('text-center'),
+            
+        ];
+
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename(): string
+    {
+        return 'Channel_' . date('YmdHis');
+    }
+}
---
+++ src/views/order/create.blade.php
@@ -0,0 +1,143 @@
+
+
+ 
+<div class="h-100" style="width:99%">		
+ 
+    <div class="row justify-content-center">
+        <div class="col-md-12 pr-0 pl-0 pt-0 pb-0">
+             <div class="card">
+             
+	<div class="card-header pr-0">
+                <div class="row align-items-center w-100">
+                    <div class="col text-left">
+                        <div class="d-flex align-self-center">
+                        Create Channel
+                        </div>
+                    </div>
+                    <div class="col-auto text-right mr-0 pr-0">
+                        <div class="btn-toolbar justify-content-end">
+                            <button class="btn btn-sm btn-danger mr-0" type="button" onClick="CLOSE(); return false;"><i class="fa fa-window-close"></i> Close</button>
+                        </div>
+                    </div>
+                </div>
+                </div>
+	<div class="card-body">
+				
+<form onSubmit="STORE(); return false;">
+
+<div id="result"></div>
+
+<div class="form-group">
+	<label for="name">Name :</label>
+	<input type="text" id="name" name="name" class="form-control" placeholder="Name" autocomplete="off">
+</div> 
+
+<div class="form-group">
+	<label for="description">Description :</label>
+    <textarea class="form-control tinymce" id="description" name="description" rows="3"></textarea>
+</div>
+
+
+<div class="form-group">
+    <label for="invoice">Invoice</label>
+    <select class="form-control" id="invoice">
+      
+      
+      <option value="1">Before the tour</option>
+      <option value="2">After the tour</option>
+      
+    </select>
+  </div>
+
+	<button id="submit" type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Save</button>
+	</form>
+	</div>
+</div>       
+		
+        
+        		
+        </div>
+    </div>
+
+</div>
+
+<script>
+$( document ).ready(function() {
+    	tinymce.init({
+  		selector: 'textarea.tinymce',
+ 		height: 200,
+  		menubar: false,
+  		plugins: [
+    	'advlist autolink lists link image charmap print preview anchor',
+    	'searchreplace visualblocks code fullscreen',
+    	'insertdatetime media table paste code help wordcount'
+  		],
+  		toolbar: 'undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+  		content_css: [
+    	'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    	'//www.tiny.cloud/css/codepen.min.css'
+  		]
+		});	
+});
+
+function CLOSE()
+{
+	tinymce.remove();
+	$.fancybox.close();
+}
+</script>
+
+<script language="javascript">
+function STORE()
+{
+	tinymce.triggerSave();
+	
+	var error = false;
+	$("#submit").attr("disabled", true);
+	$('#submit').html('<i class="fa fa-spinner fa-spin"></i>');
+	var input = ["name","description"];
+	
+	$.each(input, function( index, value ) {
+  		$('#'+ value).removeClass('is-invalid');
+  		$('#span-'+ value).remove();
+	});
+	
+	$.ajax({
+		data: {
+        	"_token": $("meta[name=csrf-token]").attr("content"),
+			"name": $('#name').val(),
+			"description": $('#description').val(),
+			"invoice": $('#invoice').val()
+        },
+		type: 'POST',
+		url: '{{ route('route_tourcms_channel.store') }}'
+		}).done(function( data ) {
+			
+			if(data.id=="1")
+			{
+				
+       				$('#dataTableBuilder').DataTable().ajax.reload( null, false );
+					$("#result").empty().append('<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button><b>Success!</b></div>').hide().fadeIn();
+       				setTimeout(function (){
+       					tinymce.remove();
+  						$.fancybox.close();
+					}, 1000);
+			}
+			else
+			{
+				$.each( data, function( index, value ) {
+					$('#'+ index).addClass('is-invalid');
+						if(value!="")
+						{
+							$('#'+ index).after('<span id="span-'+ index  +'" class="invalid-feedback" role="alert"><strong>'+ value +'</strong></span>');
+						}
+					});
+				$("#submit").attr("disabled", false);
+				$('#submit').html('<i class="fa fa-save"></i> {{ __('Save') }}');
+			}
+		});
+	
+	
+	return false;
+}
+</script>
