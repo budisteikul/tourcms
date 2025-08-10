8882612d4015f74344cc2202fbb780f84b601dfc
Budi <aku@budi.my.id>
2025-08-10 14:15:43 +0700

    Update edit.blade.php

--- src/views/contact/edit.blade.php
+++ src/views/contact/edit.blade.php
@@ -144,8 +144,14 @@
 
 <div class="tab-pane fade pt-4" id="nav-menu100" role="tabpanel" aria-labelledby="nav-menu100-tab">
                     <button type="button" class="btn btn-danger mb-2" id="template3001"  onclick="sendTemplate(3001,12); return false;"><i class="fas fa-paper-plane"></i> Kalika Jogja Night Food Tour</button>
+                    <button type="button" class="btn btn-danger mb-2" id="template3001"  onclick="sendTemplate(3001,13); return false;"><i class="fas fa-paper-plane"></i> Anisa Jogja Night Food Tour</button>
                     <button type="button" class="btn btn-danger mb-2" id="template3001"  onclick="sendTemplate(3001,55); return false;"><i class="fas fa-paper-plane"></i> Jasmine Jogja Night Food Tour</button>
                     <button type="button" class="btn btn-danger mb-2" id="template3001"  onclick="sendTemplate(3001,56); return false;"><i class="fas fa-paper-plane"></i> Dhea Jogja Night Food Tour</button>
+
+                    <button type="button" class="btn btn-danger mb-2" id="template3002"  onclick="sendTemplate(3002,12); return false;"><i class="fas fa-paper-plane"></i> Kalika Jogja Morning Food Tour</button>
+                    <button type="button" class="btn btn-danger mb-2" id="template3002"  onclick="sendTemplate(3002,13); return false;"><i class="fas fa-paper-plane"></i> Anisa Jogja Morning Food Tour</button>
+                    <button type="button" class="btn btn-danger mb-2" id="template3002"  onclick="sendTemplate(3002,55); return false;"><i class="fas fa-paper-plane"></i> Jasmine Jogja Morning Food Tour</button>
+                    <button type="button" class="btn btn-danger mb-2" id="template3002"  onclick="sendTemplate(3002,56); return false;"><i class="fas fa-paper-plane"></i> Dhea Jogja Morning Food Tour</button>
                     
 </div>
 
